e404b0ccf98de63fe574c58ebc0ed395dd66e2ec
nikitronpatron <nikita.borsh9876@gmail.com>
2023-01-30 17:04:59 +0300

    HWPY8

--- HW.py
+++ HW.py
@@ -2,13 +2,12 @@
 import codecs
 
 def Import():
-    print("Введите имя, фамилию, телефон и описание через enter: ")
+    print("Введите имя, фамилию, класс ученика через enter: ")
     name = input()
     surname = input()
-    number = int(input())
-    description = input()
+    number = input()
     dataList = codecs.open("Data.txt", "a+", "utf-8")
-    dataList.write(f"*{name} {surname}: {number}. {description}*\n")
+    dataList.write(f"*{name} {surname}, {number}.*\n")
     dataList.close()
 
 def Export():
@@ -17,12 +16,43 @@ def Export():
     print(export)
     dataList.close()
 
+def Class():
+    clas = input("Введите номер класса: ")
+    dataList = codecs.open("Data.txt", "r", "utf-8")
+    lst = dataList.readlines()
+    for i in lst:
+        if clas in i:
+            print(i)
+
+def StudentName():
+    clas = input("Введите имя ученика: ")
+    dataList = codecs.open("Data.txt", "r", "utf-8")
+    lst = dataList.readlines()
+    for i in lst:
+        if clas in i:
+            print(i)
+
+def StudentSurname():
+    clas = input("Введите фамилию ученика: ")
+    dataList = codecs.open("Data.txt", "r", "utf-8")
+    lst = dataList.readlines()
+    for i in lst:
+        if clas in i:
+            print(i)
+
+
 def Main():
-    start = int(input("Введите номер операции, которую необходимо выполнить:\n1. Добавить нового абонента.\n2. Вывести список всех абонентов.\n"))
+    start = int(input("Введите номер операции, которую необходимо выполнить:\n1. Добавить нового ученика.\n2. Вывести список всех учеников.\n3. Вывести учеников класса.\n4. Вывести учеников по имени.\n5. Вывести учеников по фамилии.\n"))
     if start == 1:
         inPut = Import()
     elif start == 2:
         outPut = Export()
+    elif start == 3:
+        cl = Class()
+    elif start == 4:
+        sName = StudentName()
+    elif start == 5:
+        sSurname = StudentSurname()
     else:
         print("Ошибка!")
 Main()
